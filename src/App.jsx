068020fddb205c059fab98ece984c245f2357232
Sihagyashika <160014955+Sihagyashika@users.noreply.github.com>
2025-04-18 22:04:35 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+// import Login from "./Component/Login";
+// import "./style/Login.css";
+import BMICalculator from "./Component/BMICalculator";
+import "./style/BMICalculator.css"
+
+function App() {
+  return (
+    <div>
+      {/* <Login /> */}
+      <BMICalculator/>
+    </div>
+  );
+}
+
+export default App;
