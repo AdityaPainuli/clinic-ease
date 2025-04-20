a94485f08f46e11eafa57cd5dde79b9d669e941d
Aditya painuli <adityapainuli2004@gmail.com>
2025-04-20 18:33:54 +0530

    added BE and FE both

---
+++ BE/src/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const authRoutes = require('./routes/auth');
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use('/api/auth', authRoutes);
+
+app.get("/health", (req, res) => {
+    console.log("hello world")
+    res.json({"status":"healthy!"})
+} )
+
+// MongoDB connection
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('MongoDB Connected'))
+.catch((err) => console.log(err));
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
